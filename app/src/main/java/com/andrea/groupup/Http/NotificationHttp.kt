276cf757d21ff15de4989c71ce3ae642f28d35a9
CapelYohann <ieauhann@gmail.com>
2020-07-05 17:13:13 +0200

    notif log

--- app/src/main/java/com/andrea/groupup/Http/NotificationHttp.kt
+++ app/src/main/java/com/andrea/groupup/Http/NotificationHttp.kt
@@ -1,10 +1,13 @@
 package com.andrea.groupup.Http
 
 import android.content.Context
+import android.util.Log
 import com.andrea.groupup.Constants
 import com.andrea.groupup.Models.MeetingPoint
 import com.andrea.groupup.Models.Notification
 import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import org.json.JSONObject
 
 class NotificationHttp (val context: Context) {
@@ -12,6 +15,7 @@ class NotificationHttp (val context: Context) {
     private val http = Http(context)
 
     fun send(notification: Notification, token: String, volleyCallback: VolleyCallback) {
+        Log.d("HTTP", "NotificationHttp - send")
         http.postWithToken(URL, volleyCallback,  JSONObject(Gson().toJson(notification)), token)
     }
 }
