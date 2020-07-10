7e6c7dbbb3f5bec4a7e53f38ac4e8eb1e7f75a24
Kyoya27 <mouadh95@live.fr>
2020-07-11 00:22:01 +0200

    photo allow share

--- app/src/main/java/com/andrea/groupup/Models/Photo.kt
+++ app/src/main/java/com/andrea/groupup/Models/Photo.kt
@@ -2,8 +2,8 @@ package com.andrea.groupup.Models
 
 import java.io.Serializable
 
-class Photo(var id: String, var link: String) : Serializable {
+class Photo(var id: String, var link: String, var allow_share: Boolean) : Serializable {
     override fun toString(): String {
-        return "Photo(id='$id', link='$link')"
+        return "Photo(id='$id', link='$link', allowshare='$allow_share')"
     }
 }
